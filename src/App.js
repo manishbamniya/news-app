af6d81c34a42e592229969e9f27b6a39ff7650d4
Dhruv <dhruv_v@remitr.com>
2021-10-12 11:23:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>You'll be provided with the latest news here...............</h3>
+    </div>
+  );
+}
+
+export default App;
